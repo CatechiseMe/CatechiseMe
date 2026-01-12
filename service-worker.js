5c943f99dd7b65f7fb9acc11c9bf4764d4d4ca69
CatechiseMe <Catechiseme@gmail.com>
2026-01-12 13:00:24 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'catechisme-cache-v2';
+const CACHE_NAME = 'catechisme-cache-v3';
 const urlsToCache = [
   './',
   './index.html',
@@ -35,4 +35,5 @@ self.addEventListener('fetch', (event) => {
 
 self.addEventListener('message', (event) => {
   if (event.data.type === 'SKIP_WAITING') self.skipWaiting();
+
 });
